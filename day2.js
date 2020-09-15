15e8280cfd6cd20a1d324e4ae0bc6f1cd8b356d7
Seantay <risingcinders@gmail.com>
2020-09-15 10:32:33 -0500

    Group Practice

--- day2.js
+++ day2.js
@@ -1,18 +1,37 @@
-// data type focused
-// strings
-"Text"
-'text'
-`text`
+// // data type focused
+// // strings
+// "Text";
+// "text";
+// `text`;
 
+// // numbers
+// 12;
+// 22;
+// 1.25;
+// 0 - 50;
 
-// numbers
-12
-22
-1.25
-0
--50
+// // booleans
+// true;
+// false;
 
-// booleans
-true
-false
+// Variables
+var x = 2;
+var y = "string";
+var z = true;
+var evennumbers = [];
 
+// console.log(y);
+
+for (i = 0; i<10; i++) {
+    x++; 
+    if (x%2 == 0) {
+        console.log( x + " is even" );
+        evennumbers.push(x);
+    }
+}
+
+console.log(evennumbers)
+
+for ( i = 0; i<2; i++) {
+    console.log("beep")
+}
